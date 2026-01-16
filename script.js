3eea80532b078408cd02896ad202eae482901628
Techeee123 <techfreak1219@gmail.com>
2026-01-16 08:10:48 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// function calculateSum() {
+//     let a = 10;
+//     let b = 20;
+//     let c = a + b;
+
+//     document.getElementById("result").innerHTML = c;
+// }
+
+function addNumbers(){
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
+
+    let sum = Number(a) + Number(b);
+
+    }
+    document.getElementById("output1").innerHTML = sum;
+
+
+
